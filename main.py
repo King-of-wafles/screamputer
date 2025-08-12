2cfb0ceeeb0585cde0e7e71916a6df4a4d3c8abb
King-of-wafles <cflanglais10@gmail.com>
2025-08-12 18:02:31 -0400

    installed SpeechRecognition and made simple recording  app with template on coderivers.org

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello, world!")
+import speech_recognition as sr
+
+# Create a recognizer object
+r = sr.Recognizer()
+
+# Use the microphone as the audio source
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+try:
+    # Recognize speech using Google Speech Recognition
+    text = r.recognize_google(audio)
+    print("You said: " + text)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
