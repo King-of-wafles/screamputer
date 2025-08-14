e9ecde0e09a1b02f0c21509ea7a0b835f25d55b8
King-of-wafles <cflanglais10@gmail.com>
2025-08-14 14:10:32 -0400

    Makes voice recognition in a function that can be called

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import speech_recognition as sr
 
-# Create a recognizer object
-r = sr.Recognizer()
+def getVoice():
+    # Create a recognizer object
+    r = sr.Recognizer()
+    # Use the microphone as the audio source
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+    try:
+        # Recognize speech using Google Speech Recognition
+        print("processing speech...")
+        text = r.recognize_google(audio)
+        print("You said: " + text)
+        return text
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        return "cannot understand audio error"
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        return "could not request results from service error"
 
-# Use the microphone as the audio source
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-
-try:
-    # Recognize speech using Google Speech Recognition
-    print("processing speech...")
-    text = r.recognize_google(audio)
-    print("You said: " + text)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+getVoice()
